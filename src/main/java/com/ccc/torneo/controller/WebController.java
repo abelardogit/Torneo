9800a6fde88aa6ea51da3dd249fd7bf2342efe87
harryvillanueva <harryvillanueva0814@gmail.com>
2025-12-11 13:54:33 +0100

    actualizacion de web controller

---
+++ src/main/java/com/ccc/torneo/controller/WebController.java
@@ -0,0 +1,45 @@
+package com.ccc.torneo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+
+    @GetMapping({"/","/index.html"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/usuarios.html")
+    public String usuarios() {
+        return "usuarios";
+    }
+
+    @GetMapping("/juegos.html")
+    public String juegos() {
+        return "juegos";
+    }
+
+    @GetMapping("/torneos.html")
+    public String torneos() {
+        return "torneos";
+    }
+
+
+    @GetMapping("/crear-usuario.html")
+    public String crearUsuario() {
+        return "crear-usuario";
+    }
+
+    @GetMapping("/crear-juego.html")
+    public String crearJuego() {
+        return "crear-juego";
+    }
+
+    @GetMapping("/crear-torneo.html")
+    public String crearTorneo() {
+        return "crear-torneo";
+    }
+}
